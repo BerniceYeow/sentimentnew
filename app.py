c644c83d865a0f98a1ec0350a4cb497c62afc31e
BerniceYeow <44364177+BerniceYeow@users.noreply.github.com>
2020-12-23 19:43:09 +0800

    Add files via upload
    delete power bi dashboard code

--- app.py
+++ app.py
@@ -20,8 +20,6 @@ import streamlit as st
 
 from PIL import Image
 
-preprocessing = malaya.preprocessing.preprocessing()
-
 
 
 def main():
@@ -45,47 +43,37 @@ def main():
  
         return df
     
-    st.sidebar.subheader("Choose What Do You Want To Do")
-    classifier = st.sidebar.selectbox(" ", ( "POWER BI Dashboard", "Interact with our chatbot"))
-    if classifier == 'POWER BI Dashboard':
-        import streamlit.components.v1 as components
-        from urllib.request import urlopen
-        html = urlopen("https://app.powerbi.com/view?r=eyJrIjoiZTA4NWU4MjYtOTk3Yi00N2ZhLTgwZWQtZWFhMzNkNDk1Zjk3IiwidCI6Ijk5NmQwYTI3LWUwOGQtNDU1Ny05OWJlLTY3ZmQ2Yjk3OTA0NCIsImMiOjEwfQ%3D%3D&pageName=ReportSection06db5928b6af61b2868f").read()
-        #components.html(html, width=None, height=600, scrolling=True)
-        st.markdown("""
-            <iframe width="900" height="606" src="https://app.powerbi.com/view?r=eyJrIjoiZTA4NWU4MjYtOTk3Yi00N2ZhLTgwZWQtZWFhMzNkNDk1Zjk3IiwidCI6Ijk5NmQwYTI3LWUwOGQtNDU1Ny05OWJlLTY3ZmQ2Yjk3OTA0NCIsImMiOjEwfQ%3D%3D&pageName=ReportSection06db5928b6af61b2868f" frameborder="0" style="border:0" allowfullscreen></iframe>
-            """, unsafe_allow_html=True)
 
   
-    if classifier == 'Interact with our chatbot':    
-        import pickle
-        with open('tnb_topic_classifier_svm', 'rb') as training_model:
-            topic_model = pickle.load(training_model)
 
-        model = malaya.sentiment.transformer(model = 'tiny-albert')
-        #from src import model          
-        #malay_bert = model.BertModel()
-        # eng_flair = model.Flair()
-        # eng_vader = model.Vader()
-        test = pd.DataFrame()
-        test['Positive'] = ''
-        test['Neutral'] = ''
-        test['Negative'] = ''
-        
-        st.title("Sentiment Analyzer")
-        message = st.text_area("Enter Text","Type Here ..")
-        if st.button("Analyze"):
-         with st.spinner("Analyzing the text …"):
-             result = model.predict_proba([message])
-             #result = malay_bert.predict(message)
-             message = [message]
-             topic = topic_model.predict(message)
-             #output = "Result is: Positive:" + str(result[0]) + "Neutral:" + str(result[1]) + "Negative:" + str(result[2]) + "topic is: " + str(topic)
-             output = "result is:" + str(result) + "topic is: " + str(topic)
-             st.write(output)
+    import pickle
+    with open('tnb_topic_classifier_svm', 'rb') as training_model:
+        topic_model = pickle.load(training_model)
 
-        else:
-         st.warning("Not sure! Try to add some more words")
+    model = malaya.sentiment.transformer(model = 'tiny-albert')
+    #from src import model          
+    #malay_bert = model.BertModel()
+    # eng_flair = model.Flair()
+    # eng_vader = model.Vader()
+    test = pd.DataFrame()
+    test['Positive'] = ''
+    test['Neutral'] = ''
+    test['Negative'] = ''
+    
+    st.title("Sentiment Analyzer")
+    message = st.text_area("Enter Text","Type Here ..")
+    if st.button("Analyze"):
+     with st.spinner("Analyzing the text …"):
+         result = model.predict_proba([message])
+         #result = malay_bert.predict(message)
+         message = [message]
+         topic = topic_model.predict(message)
+         #output = "Result is: Positive:" + str(result[0]) + "Neutral:" + str(result[1]) + "Negative:" + str(result[2]) + "topic is: " + str(topic)
+         output = "result is:" + str(result) + "topic is: " + str(topic)
+         st.write(output)
+
+    else:
+     st.warning("Not sure! Try to add some more words")
 
 
 
